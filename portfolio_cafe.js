febc7ea256d3020b86fc28d7ce23c27d3a50f7bd
NAGISA-KASE <38846918+NAGISA-KASE@users.noreply.github.com>
2018-06-21 10:06:14 +0900

    Update portfolio_cafe.js

--- portfolio_cafe.js
+++ portfolio_cafe.js
@@ -1,13 +1,32 @@
 $(function(){
 
+	// access&hoursへのページ内ジャンプをスクロールアニメーションにする
+	$('#jump').click(function() {
+		let position = $('#access-hours-section').offset().top;
+		$('html, body').animate( {
+			'scrollTop': position
+		}, 'slow');
+	})
+
+	// スマホ用メニューモーダル
+	$('#menu-icon').click(function() {
+		if($('#menu-icon').hasClass('open')) {
+			$('#menu-icon').removeClass('open');
+			$('#menu-modal').slideUp();
+		} else {
+			$('#menu-icon').addClass('open');
+			$('#menu-modal').slideDown();
+		}
+	});
+
 	//features-boxのテキスト表示をhoverで切り替える
 	$('.features-box').hover(
 		function(){
-			var text = $(this).find('p');
+			let text = $(this).find('p');
 			$(text).fadeIn(500);
 		},
 		function(){
-			var text = $(this).find('p');
+			let text = $(this).find('p');
 			$(text).fadeOut(500);
 		}
 	);
